519390e401b430dd9572ff2cab782e8d13add1f5
razi <rezaie127@yahoo.com>
2022-06-05 02:42:13 +0430

    add splash screen

--- app/src/main/java/com/example/onlineshop/MainActivity.kt
+++ app/src/main/java/com/example/onlineshop/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.onlineshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.onlineshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        splash()
+
+    }
+
+    private fun splash() {
+        supportActionBar?.hide()
+        binding.splashIcon.alpha=0f
+        binding.splashIcon.animate().setDuration(3000).alpha(1f).withEndAction {
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            binding.fragmentContainerView.visibility= View.VISIBLE
+            binding.splashIcon.visibility= View.GONE
+            supportActionBar?.show()
+        }
+
     }
 }
