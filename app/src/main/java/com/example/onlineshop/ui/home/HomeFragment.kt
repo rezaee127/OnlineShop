1a41ce7b702ad4e04e5a07aa2e8ba729d3371eaa
razi <rezaie127@yahoo.com>
2022-06-06 21:44:09 +0430

    add 3 recycler view in HomeFragment

--- app/src/main/java/com/example/onlineshop/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/onlineshop/ui/home/HomeFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.onlineshop.databinding.FragmentHomeBinding
+import com.example.onlineshop.ui.adapters.ProductsItemAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     lateinit var binding:FragmentHomeBinding
+    val vModel:HomeFragmentViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -28,5 +31,36 @@ class HomeFragment : Fragment() {
         //return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapters()
+    }
+
+    private fun initAdapters() {
+        val adapter1=ProductsItemAdapter(){}
+        binding.rvLastProduct.adapter=adapter1
+
+        vModel.listOfProductsOrderByDate.observe(viewLifecycleOwner){
+            adapter1.submitList(it)
+
+        }
+
+        val adapter2=ProductsItemAdapter(){}
+        binding.rvMostPopular.adapter=adapter2
+
+        vModel.listOfProductsOrderByPopularity.observe(viewLifecycleOwner){
+            adapter2.submitList(it)
+        }
+
+        val adapter3=ProductsItemAdapter(){}
+        binding.rvMaxRate.adapter=adapter3
+
+        vModel.listOfProductsOrderByRating.observe(viewLifecycleOwner){
+            adapter3.submitList(it)
+        }
+
+
+    }
 
 }
