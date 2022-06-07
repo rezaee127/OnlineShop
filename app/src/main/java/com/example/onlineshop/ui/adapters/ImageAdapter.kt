043c3065ca0f5d7bc67b774fe0a2fc5bfa9e2f9e
razi <rezaie127@yahoo.com>
2022-06-07 09:28:01 +0430

    add ImageAdapter

---
+++ app/src/main/java/com/example/onlineshop/ui/adapters/ImageAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.onlineshop.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.onlineshop.R
+import com.example.onlineshop.model.Image
+
+class ImageAdapter(): ListAdapter<Image, ImageAdapter.ViewHolder>(ImageDiffCallback) {
+
+    class ViewHolder(view: View, private val context: Context) : RecyclerView.ViewHolder(view) {
+        val ivMovie = view.findViewById<ImageView>(R.id.iv_detail)
+
+        fun bind(image: Image) {
+
+            Glide.with(context)
+                .load(image.src)
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.ic_baseline_error_24)
+                .fitCenter()
+                //.circleCrop()
+                .into(ivMovie)
+        }
+    }
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.image_row_item, viewGroup, false)
+
+        return ViewHolder(view,viewGroup.context)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        viewHolder.bind(getItem(position))
+
+    }
+
+
+    object ImageDiffCallback : DiffUtil.ItemCallback<Image>() {
+        override fun areItemsTheSame(oldItem: Image, newItem: Image): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Image, newItem: Image): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+}
